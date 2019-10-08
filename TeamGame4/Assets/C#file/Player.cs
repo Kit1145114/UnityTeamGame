a1d28531233070f7957aa596d2214d8f1bdc5f8a
kbc18b23 <kbc18b23@stu.kawahara.ac.jp>
2019-10-08 15:57:18 +0900

    プレイヤーのコード変更

---
+++ TeamGame4/Assets/C#file/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Move = 3.0f;
+    CharacterController CCon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CCon = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //プレイヤー移動処理
+     
+    }
+}
