3f725326b87bf0735860af016c17d7d7c91b635f
kbc18b23 <kbc18b23@stu.kawahara.ac.jp>
2019-10-08 16:57:23 +0900

    プレイヤー処理

--- TeamGame4/Assets/C#file/Player.cs
+++ TeamGame4/Assets/C#file/Player.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float Move = 3.0f;
+    private Vector3 Player_pos; //プレイヤー
+    private float x;
+    private float z;
+
     CharacterController CCon;
+
     // Start is called before the first frame update
     void Start()
     {
+        Player_pos = GetComponent<Transform>().position;
         CCon = gameObject.GetComponent<CharacterController>();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         //プレイヤー移動処理
-     
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        Vector3 moveSpeed = Vector3.zero;
+        moveSpeed.x = x;
+        moveSpeed.z = z;
+
+        CCon.Move(moveSpeed);
     }
 }
