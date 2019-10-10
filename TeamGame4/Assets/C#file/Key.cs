dd4115ad0ad862e63b54b7eedc250fb014aa5af6
kbc18b23 <kbc18b23@stu.kawahara.ac.jp>
2019-10-10 17:45:27 +0900

    鍵の判定

--- TeamGame4/Assets/C#file/Key.cs
+++ TeamGame4/Assets/C#file/Key.cs
@@ -10,6 +10,7 @@ public class Key : MonoBehaviour
     private float z;
     private float x2;
     private float z2;
+    public float Mode = 0.3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,17 +26,23 @@ public class Key : MonoBehaviour
         Vector3 p2pos = player2.transform.position;
         float posx = (player1.transform.position.x + player2.transform.position.x) / 2;
         float posz = (player1.transform.position.z + player2.transform.position.z) / 2;
-        float A = posx + posz;
+        float X = this.transform.position.x - player1.transform.position.x;
+        float Z = this.transform.position.z - player1.transform.position.z;
         //カギの移動処理
         Vector3 m_position = Vector3.zero;
         m_position.x = posx;
         m_position.z = posz;
-        
         m_position.y += transform.position.y;
         transform.position = m_position;
+        Debug.Log(X);
+        Debug.Log(Z);
         if (transform.position.y < -10.0f)
         {
             Retry.GameRetry = true;
+        }
+        if (X > Mode || X < -Mode ||Z > Mode || Z < -Mode)
+        {
+            Player.PlayerDeath = true;
         }
     }
 }
