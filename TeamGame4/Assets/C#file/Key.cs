f31d85db97565b52773723e07a4459c16cf29939
kbc18b23 <kbc18b23@stu.kawahara.ac.jp>
2019-10-10 17:05:24 +0900

    キー

--- TeamGame4/Assets/C#file/Key.cs
+++ TeamGame4/Assets/C#file/Key.cs
@@ -19,35 +19,20 @@ public class Key : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //GameObject player1 = GameObject.Find("Player");
-        //GameObject player2 = GameObject.Find("Player2");
-        //Vector3 p1pos = player1.transform.position;
-        //Vector3 p2pos = player2.transform.position;
-        //float posx = (player1.transform.position.x + player2.transform.position.x) / 2;
-        //float posz = (player1.transform.position.z + player2.transform.position.z) / 2;
+        GameObject player1 = GameObject.Find("Player");
+        GameObject player2 = GameObject.Find("Player2");
+        Vector3 p1pos = player1.transform.position;
+        Vector3 p2pos = player2.transform.position;
+        float posx = (player1.transform.position.x + player2.transform.position.x) / 2;
+        float posz = (player1.transform.position.z + player2.transform.position.z) / 2;
+        float A = posx + posz;
         //カギの移動処理
-        x = Input.GetAxis("Horizontal");
-        x2 = Input.GetAxis("Horizontal3");
-        z = Input.GetAxis("Vertical");
-        z2 = Input.GetAxis("Vertical3");
-        if (x != 0.0f || z != 0.0f)
-        {
-            Vector3 moveSpeed = Vector3.zero;
-            moveSpeed.x = (x) / MoveDown;
-            moveSpeed.z = -(z) / MoveDown;
-            transform.position += moveSpeed;
-        }
-        else if (x2 != 0.0f || z2 != 0.0f)
-        { Vector3 moveSpeed2 = Vector3.zero;
-            moveSpeed2.x = (x2) / MoveDown;
-            moveSpeed2.z = -(z2) / MoveDown;
-            transform.position += moveSpeed2;
-        }
-        //Vector3 m_position = Vector3.zero;
-        //m_position.x = x;
-        //m_position.z = z;
-        //m_position.y = 1.2f;
-        //transform.position = m_position;
+        Vector3 m_position = Vector3.zero;
+        m_position.x = posx;
+        m_position.z = posz;
+        
+        m_position.y += transform.position.y;
+        transform.position = m_position;
         if (transform.position.y < -10.0f)
         {
             Retry.GameRetry = true;
