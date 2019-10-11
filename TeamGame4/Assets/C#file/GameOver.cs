3ef865dca5b3c208bb48b5bba475743683d9c312
wada <1>
2019-10-11 14:45:25 +0900

    kakak

---
+++ TeamGame4/Assets/C#file/GameOver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject gameover;
+    public static bool gameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver)
+        {
+            gameover.SetActive(true);
+            if (Input.GetKeyDown("joystick button 0"))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
---
+++ TeamGame4/Assets/C#file/TextureChenge.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureChenge : MonoBehaviour
+{
+    //Material material;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    material = (Material)Resources.Load("Asstes/Texture/kiyohara's meigen");
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if(Input.GetButtonDown("A"))
+    //    {
+    //        GetComponent<Renderer>().material = material;
+    //    }
+    //}
+
+    public Material[] _material;           // 割り当てるマテリアル.
+    private int i;
+
+    // Use this for initialization
+    void Start()
+    {
+        i = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("joystick button 0"))
+        {
+            i++;
+            if (i == 3)
+            {
+                i = 0;
+            }
+
+            this.GetComponent<Renderer>().material = _material[i];
+        }
+
+    }
+}
