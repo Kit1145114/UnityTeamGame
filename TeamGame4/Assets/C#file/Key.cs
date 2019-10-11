0533c6a670f2da2222618845eb446f5c66f5f389
kbc18b23 <kbc18b23@stu.kawahara.ac.jp>
2019-10-11 12:06:23 +0900

    鍵の仕様変更

--- TeamGame4/Assets/C#file/Key.cs
+++ TeamGame4/Assets/C#file/Key.cs
@@ -6,18 +6,19 @@ public class Key : MonoBehaviour
 {
     public float MoveDown = 20.0f;
     private Vector3 KeyPos; //プレイヤー
+    private bool Down = false;
     private float x;
     private float z;
     private float x2;
     private float z2;
     public float Mode = 0.3f;
-    //CapsuleCollider cc;
-    MeshRenderer mr;
+    CapsuleCollider cc;
+    //MeshRenderer mr;
     // Start is called before the first frame update
     void Start()
     {
-        //cc = GetComponent<CapsuleCollider>();
-        mr = GetComponent<MeshRenderer>();
+        cc = GetComponent<CapsuleCollider>();
+       // mr = GetComponent<MeshRenderer>();
         KeyPos = GetComponent<Transform>().position;
     }
 
@@ -36,18 +37,27 @@ public class Key : MonoBehaviour
         Vector3 m_position = Vector3.zero;
         m_position.x = posx;
         m_position.z = posz;
-        m_position.y += this.transform.position.y;
-        transform.position = m_position;
+        if (X > Mode || X < -Mode || Z > Mode || Z < -Mode)
+        {
+            Down = true;
+            //cc.isTrigger = true;
+            //m_position.y -= 1.0f;
+            //mr.enabled = false;
+        }
+        else
+        {
+            m_position.y += this.transform.position.y;
+            transform.position = m_position;
+        }
         Debug.Log(X);
         Debug.Log(Z);
         if (transform.position.y < -5.0f)
         {
             Retry.GameRetry = true;
         }
-        if(X > Mode || X < -Mode ||Z > Mode||Z < -Mode)
+        if(Down)
         {
-            mr.enabled = false;
+            cc.isTrigger = true;
         }
-        
     }
 }
