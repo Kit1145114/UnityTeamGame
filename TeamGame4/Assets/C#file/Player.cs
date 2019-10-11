3abed2c4a21da6f6729fa8d0c99819afa2937ae1
kbc18b23 <kbc18b23@stu.kawahara.ac.jp>
2019-10-11 14:09:39 +0900

    ゲームオーバーの文字入れ

--- TeamGame4/Assets/C#file/Player.cs
+++ TeamGame4/Assets/C#file/Player.cs
@@ -27,23 +27,32 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //プレイヤー移動処理
-        x = Input.GetAxis("Horizontal");
-        z = Input.GetAxis("Vertical");
-
-        Vector3 moveSpeed = Vector3.zero;
-        moveSpeed.x = x / MoveDown;
-        moveSpeed.z = -(z / MoveDown);
-        transform.position += moveSpeed;
-        //CCon.Move(moveSpeed);
-
-        if (gameObject.transform.position.y <= -10.0f)
+        if (PlayerDeath == false && Player2.PlayerDeath == false)
         {
-            Retry.GameRetry = true;
+            //プレイヤー移動処理
+            x = Input.GetAxis("Horizontal");
+            z = Input.GetAxis("Vertical");
+
+            Vector3 moveSpeed = Vector3.zero;
+            Vector3 m_position = Vector3.zero;
+            moveSpeed.x = x / MoveDown;
+            moveSpeed.z = -(z / MoveDown);
+            transform.position += moveSpeed;
+            //CCon.Move(moveSpeed);
+            m_position.y = -5.0f;
+            m_position.x = transform.position.x;
+            m_position.z = transform.position.z;
+            if (gameObject.transform.position.y <= -5.0f)
+            {
+                //Retry.GameRetry = true;
+                transform.position = m_position;              
+                PlayerDeath = true;
+            }
         }
-        if(PlayerDeath == true)
+        else if(PlayerDeath == true)
         {
-            Retry.GameRetry = true;
+            //Retry.GameRetry = true;
+            GameOver.gameOver = true;
         }
     }
     private void LateUpdate()
--- TeamGame4/Assets/C#file/Stone.cs
+++ TeamGame4/Assets/C#file/Stone.cs
@@ -6,24 +6,33 @@ public class Stone : MonoBehaviour
 {
 
     private Vector3 StonePos; //岩の座標  
-
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
-    { 
+    {
+        //SphereCollider sc = GetComponent<SphereCollider>();
+        rb = GetComponent<Rigidbody>();
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        //岩の座標を持ってくる
-        StonePos = GetComponent<Transform>().position;
-        //Yが-50以下になったら
-        if (StonePos.y <= -50.0f)
+        if (Player.PlayerDeath == false && Player2.PlayerDeath == false)
         {
-            //初期位置に戻す
-            StonePos = new Vector3(0.04f, 5.48f, 37.86f);
-            transform.position = StonePos;
+            //岩の座標を持ってくる
+            StonePos = GetComponent<Transform>().position;
+            //Yが-50以下になったら
+            if (StonePos.y <= -50.0f)
+            {
+                //初期位置に戻す
+                StonePos = new Vector3(0.04f, 5.48f, 37.86f);
+                transform.position = StonePos;
+            }
+        }
+        else
+        {
+            rb.isKinematic = true;
         }
     }
 }
