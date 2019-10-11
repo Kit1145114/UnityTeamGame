b38422b3e240744435e0d521f91942e92919c52d
oomori akihiro <kbc18b05@stu.kawahara.ac.jp>
2019-10-11 12:30:12 +0900

    効果音追加

--- TeamGame4/Assets/C#file/Result.cs
+++ TeamGame4/Assets/C#file/Result.cs
@@ -5,11 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Result : MonoBehaviour
 {
+    public AudioClip se2;
+    AudioSource audioSource;
     public static bool GameCrear; 
     // Start is called before the first frame update
     void Start()
     {
         GameCrear = true;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -18,6 +21,7 @@ public class Result : MonoBehaviour
         if (Input.GetKeyDown("joystick button 0"))
         {
             SceneManager.LoadScene("Title");
+            audioSource.PlayOneShot(se2);
         }
     }
 }
